dcda333d7c2a18947d7bdcadc5b036678488182c
Charlie Weber <charlestweber@gmail.com>
2021-10-06 13:19:22 -0700

    add restaurant.cs, homecontroller, and first routes
    Co-authored-by: Kevin Funez <kevin.funez315@gmail.com>

---
+++ Restaurant/Models/Restaurant.cs
@@ -0,0 +1,12 @@
+
+namespace Restuarant.Models
+{
+  public class Restaurant
+  {
+    public int ItemId {get;set;}
+    public string Name {get; set;}
+    public int Capacity {get;set;}
+    public string OutdoorSeating {get;set;}
+    public int CuisineId {get;set;}
+  }
+}
